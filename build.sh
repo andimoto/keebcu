8fb2b9d970ce53a1c96ac56be265244e620b7ca4
Andreas <andimoto@posteo.de>
2021-03-14 22:55:16 +0100

    build.sh und template_to_copy.conf aktualisiert

--- build.sh
+++ build.sh
@@ -26,34 +26,38 @@ confFile=conf/$keebcu.conf
 echo "scad File: $scadFile"
 echo "config File: $confFile"
 
-layout=`cat $confFile | grep layout | cut -d : -f 2`
-if [ -z $layout ]
-then
-  echo "No layout found in config file! Exit..."
-  echo "*** Set Variable 'layout=<varName>' in conf/<config file> ***"
-  echo "*** varName should be the name of the array which describe the layout ***"
-  exit 0
-else
-  echo "Building layout $layout..."
-fi
+# get line until print out scad file into temporary scad file
+LINE=`grep -n BUILD_LINE $scadFile | cut -d : -f 1`
 
+# get layout out of config file
+# layout=`cat $confFile | grep layout | cut -d : -f 2`
+# if [ -z $layout ]
+# then
+#   echo "No layout found in config file! Exit..."
+#   echo "*** Set Variable 'layout=<varName>' in conf/<config file> ***"
+#   echo "*** varName should be the name of the array which describe the layout ***"
+#   exit 0
+# else
+#   echo "Building layout $layout..."
+# fi
+
+# get all modules to build
 modules=`cat conf/template_to_copy.conf | grep module | cut -d :  -f 2`
 for i in $modules; do
     echo module: $i
 done
 
+# create temporary directory for build
 outputDir=stl/$keebcu
 tempDir=temp$RANDOM
 mkdir $tempDir
 
-
-
-LINE=`grep -n BUILD_LINE $scadFile | cut -d : -f 1`
-# echo $LINE
-head -n $LINE $scadFile > $tempDir/temp$scadFile
-
-# TEST
-echo "mainCase(template_ansiUS_7583);" >> $tempDir/temp$scadFile
-
+for i in $modules; do
+    echo calling module: $i
+		head -n $LINE $scadFile > $tempDir/temp$scadFile
+		echo $i >> $tempDir/temp$scadFile
+		outputFile=`echo "$i" | cut -d \( -f 1`
+		echo $keebcu\_$outputFile
+done
 # remove temprary directory
-rm -rf $tempDir
+# rm -rf $tempDir
